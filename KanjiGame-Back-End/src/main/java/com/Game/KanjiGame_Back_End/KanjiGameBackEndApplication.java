7bb0ef2b8381a50d15dc87834a02133732146d27
João Albieri <130722417+Xua1zin@users.noreply.github.com>
2024-10-02 12:17:56 -0300

    Add files via upload

---
+++ KanjiGame-Back-End/src/main/java/com/Game/KanjiGame_Back_End/KanjiGameBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.Game.KanjiGame_Back_End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiGameBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanjiGameBackEndApplication.class, args);
+	}
+
+}
---
+++ KanjiGame-Back-End/src/test/java/com/Game/KanjiGame_Back_End/KanjiGameBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Game.KanjiGame_Back_End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjiGameBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
